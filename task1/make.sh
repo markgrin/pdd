c504b514599559c8e35f748d2c1fea3663b0c3ab
Lev Melnikov <lew.ka@hotmail.com>
2020-10-15 09:06:43 +0300

    final makefile

--- task1/make.sh
+++ task1/make.sh
@@ -1,1 +1,14 @@
-g++ -O3 -lpthread main.cpp
+#!/bin/bash
+
+CMD=g++ # Компилятор
+CMD="$CMD -O3" # Максимальная оптимизация
+CMD="$CMD -lpthread" # Линкуем библиотеку
+CMD="$CMD -fvisibility=hidden" # Избавляемся от ненужных символов
+CMD="$CMD -fvisibility-inlines-hidden" # Избавляемся от ненужных символов
+CMD="$CMD -s" # Избавляемся от ненужных символов
+CMD="$CMD main.cpp" # Файл с кодом
+CMD="$CMD -o ./prog.out" # Избавляемся от символов
+
+echo "Executing $CMD && strip ./prog.out"
+$CMD && strip ./prog.out
+
